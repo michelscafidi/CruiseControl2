9535125c172b7bfe6b1ab67a8bffc1b7d61fb1fc
michelscafidi <Michelangelo.Scafidi@GENHQ014.geneca.com>
2013-08-03 15:47:33 -0500

    original

--- CSharp/Commander.cs
+++ CSharp/Commander.cs
@@ -24,74 +24,9 @@ namespace CruiseControl
         {
             var cmds = new List<Command>();
 			//comment
-			foreach (var vesselStatus in _currentBoard.MyVesselStatuses)
-			{
-				_myVessels.Add(vesselStatus);
-			}
-			var vesselCount = _myVessels.Count();
+			
 
 
-			if (_currentBoard.TurnsUntilBoardShrink == 2)
-			{
-				foreach (var vessel in _myVessels)
-				{
-					foreach (var location in vessel.Location)
-					{
-						if (location.X == _currentBoard.BoardMinCoordinate.X &&
-							(location.Y == _currentBoard.BoardMinCoordinate.Y || location.Y == _currentBoard.BoardMaxCoordinate.Y))
-						{
-							cmds.Add(new Command { vesselid = vessel.Id, action = "move:east" });
-							_log[_log.Count()] = "left corners";
-						}
-						if (location.X == _currentBoard.BoardMaxCoordinate.X &&
-							(location.Y == _currentBoard.BoardMinCoordinate.Y || location.Y == _currentBoard.BoardMaxCoordinate.Y))
-						{
-							cmds.Add(new Command { vesselid = vessel.Id, action = "move:west" });
-							_log[_log.Count()] = "right corners";
-						}
-					}
-				}
-			}
-			if (_currentBoard.TurnsUntilBoardShrink == 1)
-			{
-				foreach (var vessel in _myVessels)
-				{
-					bool doneX = false, doneY = false;
-					foreach (var location in vessel.Location)
-					{
-						if (!doneX)
-						{
-							if (location.X == _currentBoard.BoardMinCoordinate.X)
-							{
-								cmds.Add(new Command { vesselid = vessel.Id, action = "move:east" });
-								doneX = true;
-								_log[_log.Count()] = "left edge";
-							}
-							if (location.X == _currentBoard.BoardMaxCoordinate.X)
-							{
-								cmds.Add(new Command { vesselid = vessel.Id, action = "move:west" });
-								doneX = true;
-								_log[_log.Count()] = "right edge";
-							}
-						}
-						if (!doneY)
-						{
-							if (location.Y == _currentBoard.BoardMinCoordinate.Y)
-							{
-								cmds.Add(new Command { vesselid = vessel.Id, action = "move:south" });
-								doneY = true;
-								_log[_log.Count()] = "top edge";
-							}
-							if (location.Y == _currentBoard.BoardMaxCoordinate.Y)
-							{
-								cmds.Add(new Command { vesselid = vessel.Id, action = "move:north" });
-								doneY = true;
-								_log[_log.Count()] = "bottom edge";
-							}
-						}
-					}
-				}
-			}
 
             // Add Commands Here.
             // You can only give as many commands as you have un-sunk vessels. Powerup commands do not count against this number. 
